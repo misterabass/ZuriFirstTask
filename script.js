86fad62d550ae89d3a98cfc44ba95a5df954bc1a
Apalowo Abass <114651037+misterabass@users.noreply.github.com>
2023-09-09 16:30:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function updateData() {
     const dayOfWeek = now.toLocaleDateString('en-us', {weekday:'long'})
     const utcTime = now.getTime() 
 
-    slackUserNameElement.textContent = 'misterabass'
-    currentDayOfTheWeekElement.textContent = dayOfWeek
-    currentUTCTimeElement.textContent = utcTime
-    myTrackElement.textContent = 'Frontend'
+    slackUserNameElement.textContent += 'misterabass'
+    currentDayOfTheWeekElement.textContent += dayOfWeek
+    currentUTCTimeElement.textContent += utcTime
+    myTrackElement.textContent += 'Frontend'
 }
 window.onload = updateData
