b9dc1adc92f467bc0b3ee4db6220d6efacbf7313
Apalowo Abass <114651037+misterabass@users.noreply.github.com>
2023-09-09 17:04:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,26 @@ function updateData() {
     currentDayOfTheWeekElement.textContent +=  " " + dayOfWeek
     currentUTCTimeElement.textContent +=  " " + utcTime
     myTrackElement.textContent +=  " " + 'Frontend'
+
+    function updateClock() {
+        const now = new Date();
+        const seconds = now.getSeconds();
+        utcTimeElement.textContent = now.getTime();
+        lastUpdatedTimeElement.textContent = now.toISOString();
+
+        
+        const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+        
+        currentUTCTimeElement.textContent = `UTC Time: ${now.getHours()}:${now.getMinutes()}:${formattedSeconds}`;
+    }
+
+    
+    updateClock();
+
+    
+    setInterval(updateClock, 1000);
+
+
 }
 window.onload = updateData
